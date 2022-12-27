19996f398ac2e4926cbb94664326128d6be59df2
2022188 <121483328+2022188@users.noreply.github.com>
2022-12-27 19:30:14 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function inform(){
+    alert("나는 alert 입니다.");
+}
+
+function grade(){
+    var kor1 = document.getElementById(kor1).innerHTML;
+    var kor2 = document.getElementById(kor2).innerHTML;
+    document.getElementById("first_g").innerHTML=kor1+kor2;
+
+    var eng1 = document.getElementById(eng1).innerHTML;
+    var eng2 = document.getElementById(eng2).innerHTML;
+    document.getElementById("second_g").innerHTML="195";
+
+    var math1 = document.getElementById(kor1).innerHTML;
+    var math2 = document.getElementById(kor2).innerHTML;
+    document.getElementById("third_g").innerHTML="195";
+}
